ef2eb9429618c1331ee674e7e115200e2e3aca5b
fernando-angeli <fernando_angeli@hotmail.com>
2024-09-24 15:54:38 -0300

    first commit - implement structure of API ERP+praTi

---
+++ src/main/java/com/erp/maisPraTi/MaisPraTiApplication.java
@@ -0,0 +1,13 @@
+package com.erp.maisPraTi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisPraTiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisPraTiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erp/maisPraTi/MaisPraTiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.maisPraTi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaisPraTiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
